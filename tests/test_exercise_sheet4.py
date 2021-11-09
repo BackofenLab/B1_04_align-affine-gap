17338275eaac38f651e31855ae18f4b018a622cc
domonik <schdruzzi@gmail.com>
2021-11-09 13:53:52 +0100

    finished tests for exercise 3

--- tests/test_exercise_sheet4.py
+++ tests/test_exercise_sheet4.py
@@ -1,5 +1,6 @@
 import pytest
 from exercise_sheet4 import *
+from math import inf
 
 
 def test_exercise_1a():
@@ -36,3 +37,73 @@ def test_exercise_3a():
     minimization, maximization = exercise_3a()
     assert minimization is False
     assert maximization is True
+
+
+def test_exercise_3b():
+    d_ij, q_ij, p_ij = exercise_3b()
+
+    #          T    A    C    G    C    A    G    A
+    expected_d_ij = [
+        [0,   -5,  -6,  -7,  -8,  -9, -10, -11, -12],
+        [-5,   1,  -4,  -5,  -6,  -7,  -8,  -9, -10],  # T
+        [-6,  -4,   1,  -3,  -5,  -5,  -7,  -8,  -9],  # C
+        [-7,  -5,  -4,   2,  -3,  -4,  -5,  -6,  -7],  # C
+        [-8,  -6,  -5,  -3,   3,  -2,  -3,  -4,  -5],  # G
+        [-9,  -7,  -5,  -4,  -2,   3,  -1,  -3,  -3],  # A
+    ]
+
+    assert d_ij == expected_d_ij
+
+    #         T    A    C    G    C    A    G    A
+    expected_q_ij = [
+        [0,   0,   0,   0,   0,   0,   0,   0,   0],
+        [-inf, -10,  -4,  -5,  -6,  -7,  -8,  -9, -10],  # T
+        [-inf, -11,  -9,  -4,  -5,  -6,  -7,  -8,  -9],  # C
+        [-inf, -12, -10,  -9,  -3,  -4,  -5,  -6,  -7],  # C
+        [-inf, -13, -11, -10,  -8,  -2,  -3,  -4,  -5],  # G
+        [-inf, -14, -12, -10,  -9,  -7,  -2,  -3,  -4],  # A
+    ]
+    assert q_ij == expected_q_ij
+    expected_p_ij = [
+        [0,-inf,-inf,-inf,-inf,-inf,-inf,-inf,-inf],
+        [0, -10, -11, -12, -13, -14, -15, -16, -17],  # T
+        [0, -4,  -9,  -10, -11, -12, -13, -14, -15],  # C
+        [0, -5,  -4,   -8, -10, -10, -12, -13, -14],  # C
+        [0, -6,  -5,   -3,  -8,  -9, -10, -11, -12],  # G
+        [0, -7,  -6,   -4,  -2,  -7,  -8,  -9, -10],  # A
+    ]
+
+    assert p_ij == expected_p_ij
+
+
+def test_exercise_3c():
+    alignments, score = exercise_3c()
+    expected_alignments = [
+        ("T---CCGA",
+         "TACGCAGA"),
+        ("TCC---GA",
+         "TACGCAGA"),
+        ("TCCG---A",
+         "TACGCAGA")
+    ]
+
+    assert score == -3
+    assert set(alignments) == set(expected_alignments)
+
+
+def test_exercise_3d():
+    """
+    The same alignments as in 3c, since the same scoring function is optimized.
+    Calculation is just less efficient.
+    """
+    alignments = exercise_3d()
+    expected_alignments = [
+        ("T---CCGA",
+         "TACGCAGA"),
+        ("TCC---GA",
+         "TACGCAGA"),
+        ("TCCG---A",
+         "TACGCAGA")
+    ]
+
+    assert set(alignments) == set(expected_alignments)
