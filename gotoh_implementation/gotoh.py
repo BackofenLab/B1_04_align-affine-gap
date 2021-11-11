36bd2c8ace15c07702d35694acabf2bc130789cb
Alexander-Mitrofanov <mitrofan@informatik.uni-freiburg.de>
2021-11-11 15:41:34 +0100

    gotoh forward

--- gotoh_implementation/gotoh.py
+++ gotoh_implementation/gotoh.py
@@ -1,23 +1,105 @@
+from typing import Dict, List, Tuple
 from math import inf
 
 
 def zero_init_correct(seq1, seq2):
     return [[0] * (len(seq2) + 1) for _ in range(len(seq1) + 1)]
 
-def init_d_matrix(seq1, seq2, scoring):
-    pass
+
+def init_d_matrix_correct(seq1, seq2, scoring):
+    match, mismatch, gap_intro, gap_extend = (
+        scoring["match"],
+        scoring["mismatch"],
+        scoring["gap_introduction"],
+        scoring["gap_extension"]
+    )
+    d_matrix = zero_init_correct(seq1, seq2)
+    first_row = [0] + [gap_intro + i * gap_extend for i in range(1, len(seq2) + 1)]
+    d_matrix[0] = first_row
+    for index, row in enumerate(d_matrix):
+        if index > 0:
+            row[0] = gap_intro + index * gap_extend
+    return d_matrix
 
 
-def init_p_matrix(seq1 ,seq2, scoring):
-    pass
+def init_p_matrix_correct(seq1, seq2):
+    p_matrix = zero_init_correct(seq1, seq2)
+    first_row = [inf for _ in range(len(seq2) + 1)]
+    if first_row:
+        first_row[0] = 0
+    p_matrix[0] = first_row
+    for index, row in enumerate(p_matrix):
+        if index == 0:
+            row[0] = 0
+        else:
+            row[0] = "-"
+    return p_matrix
 
 
-def init_q_matrix(seq1, seq2, scoring):
-    pass
+def init_q_matrix_correct(seq1, seq2):
+    q_matrix = zero_init_correct(seq1, seq2)
+    first_row = ["-" for _ in range(len(seq2) + 1)]
+    if first_row:
+        first_row[0] = 0
+    q_matrix[0] = first_row
+    for index, row in enumerate(q_matrix):
+        if index == 0:
+            row[0] = 0
+        else:
+            row[0] = inf
+    return q_matrix
 
 
-def gotoh_init(seq1, seq2, scoring):
-    d_matrix = init_d_matrix(seq1, seq2, scoring)
-    p_matrix = init_p_matrix(seq1, seq2, scoring)
-    q_matrix = init_q_matrix(seq1, seq2, scoring)
-    return
+def gotoh_init_correct(seq1, seq2, scoring):
+    d_matrix = init_d_matrix_correct(seq1, seq2, scoring)
+    p_matrix = init_p_matrix_correct(seq1, seq2)
+    q_matrix = init_q_matrix_correct(seq1, seq2)
+    return d_matrix, p_matrix, q_matrix
+
+
+def gotoh_forward_correct(seq1, seq2, scoring: Dict[str, int]):
+    match_score, mismatch_score, gap_intro, gap_extend = (
+        scoring["match"],
+        scoring["mismatch"],
+        scoring["gap_introduction"],
+        scoring["gap_extension"]
+    )
+
+    d_matrix, p_matrix, q_matrix = gotoh_init_correct(seq1, seq2, scoring)
+    for row_index, row_d in enumerate(d_matrix[1:], 1):
+        for column_index, column_d in enumerate(row_d[1:], 1):
+            p_d = d_matrix[row_index-1][column_index] + gap_intro + gap_extend
+            p_p = p_matrix[row_index-1][column_index] + gap_extend
+            p_matrix[row_index][column_index] = min(p_d, p_p)
+
+            q_d = d_matrix[row_index][column_index-1] + gap_intro + gap_extend
+            q_q = q_matrix[row_index][column_index - 1] + gap_extend
+            q_matrix[row_index][column_index] = min(q_d, q_q)
+
+            char_seq_1, char_seq_2 = seq1[row_index - 1], seq2[column_index - 1]
+            no_gap_score = match_score if char_seq_1 == char_seq_2 else mismatch_score
+            d_diagonal = d_matrix[row_index - 1][column_index - 1] + no_gap_score
+            d_p = p_matrix[row_index][column_index]
+            d_q = p_matrix[row_index][column_index]
+            d_matrix[row_index][column_index] = min(d_diagonal, d_p, d_q)
+
+            print("matrix", row_index, column_index)
+            print(d_diagonal, d_p, d_q, )
+
+    return d_matrix, p_matrix, q_matrix
+
+
+def main():
+    scoring = {"match": 0, "mismatch": 1, "gap_introduction": 4, "gap_extension": 1}
+    s1 = "CC"
+    s2 = "ACCT"
+    d, p, q = gotoh_init_correct(s1, s2, scoring)
+    d, p, q = gotoh_forward_correct(s1, s2, scoring)
+
+    print(d)
+    print(p)
+    print(q)
+
+
+if __name__ == "__main__":
+    main()
